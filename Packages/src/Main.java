ddab06de83514e38ceb6372d521f91bca79b30c3
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-18 23:55:13 +0100

    issue done. closes #2.

---
+++ Packages/src/Main.java
@@ -0,0 +1,11 @@
+import javafx.scene.Node;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Node node = null;
+
+        org.w3c.dom.Node anotherNode = null;
+
+    }
+}
---
+++ WinDemo/src/com/example/awtSample/Main.java
@@ -0,0 +1,13 @@
+package com.example.awtSample;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 4/18/2021 , 
+    CREATED ON : 11:47 PM
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
